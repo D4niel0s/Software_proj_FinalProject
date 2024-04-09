0ef56fb4aa8a77c084d074c285baa08a487ce977
D4niel0s <das.volkov@gmail.com>
2024-04-09 20:51:18 +0300

    Added chapter 1.3 and assertions to check allocations

--- symnmf.c
+++ symnmf.c
@@ -9,9 +9,11 @@
 double **computeSimMat(Point *data, int n,int d){
     double **res = (double **)malloc(sizeof(double *)*n);
     int i,j;
+    assert(res);
 
     for(i=0; i<n; ++i){
         res[i] = (double *)malloc(sizeof(double)*n);
+        assert(res[i]);
 
         for(j=0; j<n; ++j){
             if(i != j){
@@ -30,10 +32,11 @@ double **computeDegMat(double **simMat, int n){
     double **res = (double **)malloc(sizeof(double *)*n);
     double sum;
     int i,j;
-
+    assert(res);
 
     for(i=0; i<n; ++i){
         res[i] = (double *)malloc(sizeof(double)*n);
+        assert(res[i]);
         sum = 0;
 
         for(j=0; j<n; ++j){ /*Compute d[i]*/
@@ -52,7 +55,69 @@ double **computeDegMat(double **simMat, int n){
     return res;
 }
 
+/*A is the similarity matrix, and D is the diagonal degree matrix*/
+double **computeNormSimMat(double **A,double **D, int n){
+    double **normD = (double **)malloc(sizeof(double *)*n);
+    int i,j;
+    assert(normD);
 
+    for(i=0; i<n; ++i){
+        normD[i] = (double *)malloc(sizeof(double)*n);
+        assert(normD[i]);
+
+        for(j=0; j<n; ++j){
+            if(i == j){
+                normD[i][j] = 1.0/(sqrt(D[i][j]));
+            }else{
+                normD[i][j] = 0;
+            }
+        }
+    }
+
+    double **C = mulMat(normD,A,n);
+    double **res = mulMat(C,normD,n);
+
+    /*Free auxiliary allocated memory*/
+    for(i=0; i<n; ++i){
+        free(normD[i]);
+        free(C[i]);
+    }
+    free(normD);
+    free(C);
+
+    return res;
+}
+
+
+
+
+
+/*Multiplies two nxn matrices, returned value is a new matrix: A*B*/
+double **mulMat(double **A,double **B, int n){
+    double **res = (double **)malloc(sizeof(double *)*n);
+    int sum;
+    int i,j,k;
+    assert(res);
+
+    for(i=0; i<n; ++i){ /*Iterate rows of A*/
+        res[i] = (double *)malloc(sizeof(double)*n);
+        assert(res[i]);
+
+        for(j=0; j<n; ++j){ /*Iterate columns of B*/
+            sum = 0;
+            for(k=0; k<n; ++k){ /*multiply elements*/
+                sum += A[i][k] * B[k][j];
+            }
+
+            res[i][j] = sum;
+        }
+    }
+
+    return res;
+}
+
+
+/*Calculates euclidean distance between a and b, assumes matching dimensions*/
 double eucDist(Point a, Point b){
     int i;
     double res = 0;
--- symnmf.h
+++ symnmf.h
@@ -1,11 +1,14 @@
 /*INCLUDES*/
 //#include <stdlib.h>
 //#include <math.h>
+//#include <assert.h>
 
 /*PROTOTYPES*/
-double eucDist(Point,Point);
 double **computeSimMat(Point*,int,int);
 double **computeDegMat(double**,int);
+double **computeNormSimMat(double**,double**,int);
+double **mulMat(double**,double**,int);
+double eucDist(Point,Point);
 
 /*DEFINITIONS*/
 typedef struct P{
